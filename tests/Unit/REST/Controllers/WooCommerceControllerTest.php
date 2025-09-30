e153bb891039594cac5061845124b2627637cca5
imrishcodeart <imriwain@gmail.com>
2025-09-30 11:39:33 +0200

    test(phase-4): add unit tests for WC products tool and controller; docs: update README and Developer Guide for WooCommerce integration

---
+++ tests/Unit/REST/Controllers/WooCommerceControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AIAgent\Tests\Unit\REST\Controllers;
+
+use AIAgent\REST\Controllers\WooCommerceController;
+use AIAgent\Support\Logger;
+use PHPUnit\Framework\TestCase;
+
+final class WooCommerceControllerTest extends TestCase
+{
+    public function testProductsSearchDisabledWhenFeatureOff(): void
+    {
+        update_option('ai_agent_woocommerce_enabled', false);
+        $logger = new Logger();
+        $controller = new WooCommerceController($logger);
+        $request = $this->createMock(\WP_REST_Request::class);
+        $result = $controller->productsSearch($request);
+        $this->assertInstanceOf(\WP_Error::class, $result);
+        $this->assertSame('wc_disabled', $result->get_error_code());
+    }
+}
+
+
